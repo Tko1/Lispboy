032b3af6592f704488af0f4cf757d1ffca48f1f1
Cameron Cooper <zdot101@gmail.com>
2019-08-23 19:41:20 -0400

    If I do end up cleaning up this old project, I would consider this my first 'real' commit, starting by replacing gloss with quil, and removing my non idiomatic hask-tools, which was a sort of experimentation with a strange oop-esque haskellian fusion that will be replaced with just plain data everywhere.  I have to say that it is strange that I used Clojure to explore these very non-data-centric ideas, only to arrive where I started all along by now modelling EVERYTHING in a program as plain data, data that remains data, an interface for what it expresses, for as long as possible before finally commiting to an effect, being reduced to a concretion.  Anyways, hello world

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [gloss "0.2.6"]
+                 ;; This project is a bit old, this will most likely be replaced with
+                 ;; quill
                  [penumbra "0.6.0"]
+                 [quil "3.0.0"]
                  [hask-tools "0.1.0-SNAPSHOT"]]
   :main gameboy-emulator.core)
