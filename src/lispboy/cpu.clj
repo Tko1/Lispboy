bf92277c22dfc6dc7435fd02495264ac00a7948e
Cameron Cooper <zdot101@gmail.com>
2019-09-14 22:37:56 -0400

    Added opcodes, and a spec for our cpu

--- src/lispboy/cpu.clj
+++ src/lispboy/cpu.clj
@@ -1,9 +1,15 @@
 (ns lispboy.cpu
-  (:require [lispboy.simple-lenses :as l]))
+  (:require [lispboy.simple-lens :as l]
+            [clojure.java [io :as io]]
+            ;; This gives us raw-opcode-data
+            [lispboy.opcodes :refer :all]
+            [clojure.spec.alpha :as s]))
 
+(s/def ::cpu (s/keys :req-un [::a ::f ::c ::b ::e ::d ::l ::h ::sp ::pc]))
 (def cpu
-  {:a 0x0 :f 0x0
-   :c 0x0 :b 0x0
-   :e 0x0 :d 0x0
-   :l 0x0 :h 0x0
-   :sp 0x0 :pc 0x0})
+  (s/assert ::cpu
+            {:a 0x0 :f 0x0
+             :c 0x0 :b 0x0
+             :e 0x0 :d 0x0
+             :l 0x0 :h 0x0
+             :sp 0x0 :pc 0x0}))
