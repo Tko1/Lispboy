5785ca0f2407e0cbdb142b85d8d1cf0e6591a5de
Cameron Cooper <zdot101@gmail.com>
2019-10-08 00:08:04 -0400

    Remove test directory for old namespace

--- test/gameboy_emulator/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gameboy-emulator.core-test
-  (:require [clojure.test :refer :all]
-            [gameboy-emulator.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
